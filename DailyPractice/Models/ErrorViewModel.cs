958cb6e96c6eab11a5162d39ef1e14be0c0958dd
GallyamovAmir <amirgallyamov9967@gmail.com>
2023-05-14 03:36:56 +0300

    Добавьте файлы проекта.

---
+++ DailyPractice/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DailyPractice.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
